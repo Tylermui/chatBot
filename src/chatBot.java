bec3a59cc014f1e702692eccbe2be33d504729b4
Tyler mui <tylermui1103@gmail.com>
2023-01-30 10:19:06 -0500

    adding the regex patterns

--- src/chatBot.java
+++ src/chatBot.java
@@ -9,10 +9,11 @@ public class chatBot {
         Scanner scan = new Scanner(System.in);
         String input ="";
 
+        System.out.println("What do you want");
+
         while(!input.equals("quit")) {
             String response = "";
             int random = new Random().nextInt(2 - 1 + 1) + 1;
-            System.out.println("What do you want");
             input = scan.nextLine();
 
             //breaks the loop if quit is said
@@ -25,25 +26,25 @@ public class chatBot {
             Matcher m2 = p2.matcher(input);
             Pattern p3 = Pattern.compile("I need (.*)"); 
             Matcher m3 = p3.matcher(input);
-            Pattern p4 = Pattern.compile(""); 
+            Pattern p4 = Pattern.compile("Can you help me with (.*)"); 
             Matcher m4 = p4.matcher(input);
-            Pattern p5 = Pattern.compile(""); 
+            Pattern p5 = Pattern.compile("I'm looking for (.*)"); 
             Matcher m5 = p5.matcher(input);
-            Pattern p6 = Pattern.compile(""); 
+            Pattern p6 = Pattern.compile("I'm worried about (.*)"); 
             Matcher m6 = p6.matcher(input);
-            Pattern p7 = Pattern.compile(""); 
+            Pattern p7 = Pattern.compile("I think (.*)"); 
             Matcher m7 = p7.matcher(input);
-            Pattern p8 = Pattern.compile(""); 
+            Pattern p8 = Pattern.compile("I'm feeling (.*)"); 
             Matcher m8 = p8.matcher(input);
-            Pattern p9 = Pattern.compile(""); 
+            Pattern p9 = Pattern.compile("I have a problem with (.*)"); 
             Matcher m9 = p9.matcher(input);
-            Pattern p10 = Pattern.compile(""); 
+            Pattern p10 = Pattern.compile("Are you (.*)"); 
             Matcher m10 = p10.matcher(input);
-            Pattern p11 = Pattern.compile(""); 
+            Pattern p11 = Pattern.compile("Can you (.*)"); 
             Matcher m11 = p11.matcher(input);
-            Pattern p12 = Pattern.compile(""); 
+            Pattern p12 = Pattern.compile("You are (.*)"); 
             Matcher m12 = p12.matcher(input);
-            Pattern p13 = Pattern.compile(""); 
+            Pattern p13 = Pattern.compile("I'm confused about (.*)"); 
             Matcher m13 = p13.matcher(input);
             Pattern p14 = Pattern.compile(""); 
             Matcher m14 = p14.matcher(input);
@@ -61,7 +62,11 @@ public class chatBot {
             Matcher m20 = p20.matcher(input);
             //producing the proper response
             if(m1.matches()) {
-                response = "What would getting " + reflect(m1.group(1)) + " mean to you?"; 
+                if(random==1)
+                    response = "What would getting " + reflect(m1.group(1)) + " mean to you?"; 
+                else if(random==2)
+                    response = "What would " + reflect(m1.group(1)) + " accomplish for you?";
+
             }
             else if(m2.matches()) {
                 response = "Besides " + reflect(m2.group(2)) +
@@ -73,6 +78,44 @@ public class chatBot {
                 else if(random==2)
                     response = "Are you sure you really need " + reflect(m3.group(1));
             }
+            else if(m4.matches()) {
+                response = "Of course, I'd be happy to help with " + reflect(m4.group(1)) + 
+                ". What do you need specifically?";
+            }
+            else if(m5.matches()) {
+                response = "What would finding " + reflect(m5.group(1)) + " do for you?";
+            }
+            else if(m6.matches()) {
+                response = "Why are you worried about " + reflect(m6.group(1)) + "?";
+            }
+            else if(m7.matches()) {
+                response = "Why do you think " + reflect(m7.group(1)) + "?";
+            }
+            else if(m8.matches()) {
+                response = "I'm sorry to hear that you're feeling " + reflect(m8.group(1)) + 
+                    ". Is there anything I can do to help?";
+            }
+            else if(m9.matches()) {
+                response = "What is the problem you're having with " + reflect(m9.group(1)) + "?";
+            }
+            else if(m10.matches()) {
+                response = "I'm an AI, so I do not have the ability to be " + reflect(m10.group(1)) 
+                    + ". Is there something specific you need help with?";
+            }
+            else if(m11.matches()) {
+                response = "I'm not sure if I can " + reflect(m11.group(1)) 
+                    + ". Can you please provide more context?";
+            }
+            else if(m12.matches()) {
+                response = "I'm sorry, I don't understand how you perceive me as " + reflect(m12.group(1)) 
+                + ". Can you please elaborate?";
+            }
+            else if(m13.matches()) {
+                response = "Can you explain what specifically you're confused about " + reflect(m13.group(1));
+            }
+
+
+
             else { // Give default response if input matches no pattern 
                 response = "That's interesting. Tell me one of your favorite things.";
             }
@@ -80,59 +123,55 @@ public class chatBot {
             System.out.println(response);
         }
     }
-
     public static String reflect(String in) {
-        String temp = in;
+        String temp = " " + in;
+        System.out.println(temp);
+        String output = in;
+
         if(in.contains(" are "))
-            temp = StringUtils.replaceEach(temp, new String[] { " are " }, new String[] { " am " });
-        if (in.contains(" am "))
-            temp = StringUtils.replaceEach(temp, new String[] { " am " }, new String[] { " are " });
+            output = StringUtils.replaceEach(output, new String[] { " are " }, new String[] { " am " });
+        
+        //bug with you and I not reflecting at all, it has to do with the spacing
+        if(in.contains(" I "))
+            output = StringUtils.replaceEach(output, new String[] { " I " }, new String[] { " you " });
+        if(in.contains(" you "))
+            output = StringUtils.replaceEach(output, new String[] { " you " }, new String[] { " me " });
+        if(in.contains(" you "))
+            output = StringUtils.replaceEach(output, new String[] { " you " }, new String[] { " I " });
+        
+        if(in.contains(" am "))
+            output = StringUtils.replaceEach(output, new String[] { " am " }, new String[] { " are " });
         if(in.contains(" were "))
-            temp = StringUtils.replaceEach(temp, new String[] { " were " }, new String[] { " was " });
-        if(in.contains(" you "))
-            temp = StringUtils.replaceEach(temp, new String[] { " you " }, new String[] { " me " });
-        if(in.contains(" you "))
-            temp = StringUtils.replaceEach(temp, new String[] { " you " }, new String[] { " I " });
+            output = StringUtils.replaceEach(output, new String[] { " were " }, new String[] { " was " });
         if(in.contains(" you would "))
-            temp = StringUtils.replaceEach(temp, new String[] { " you would " }, new String[] { " I'd " });
+            output = StringUtils.replaceEach(output, new String[] { " you would " }, new String[] { " I'd " });
         if(in.contains(" you have "))
-            temp = StringUtils.replaceEach(temp, new String[] { " you have " }, new String[] { " I've " });
+            output = StringUtils.replaceEach(output, new String[] { " you have " }, new String[] { " I've " });
         if(in.contains(" you will "))
-            temp = StringUtils.replaceEach(temp, new String[] { " you will " }, new String[] { " I'll " });
+            output = StringUtils.replaceEach(output, new String[] { " you will " }, new String[] { " I'll " });
         if(in.contains(" your "))
-            temp = StringUtils.replaceEach(temp, new String[] { " your " }, new String[] { " my " });
+            output = StringUtils.replaceEach(output, new String[] { " your " }, new String[] { " my " });
         if(in.contains(" I have "))
-            temp = StringUtils.replaceEach(temp, new String[] { " I have " }, new String[] { " you've " });
+            output = StringUtils.replaceEach(output, new String[] { " I have " }, new String[] { " you've " });
         if(in.contains(" I will "))
-            temp = StringUtils.replaceEach(temp, new String[] { " I will " }, new String[] { " you'll " });
+            output = StringUtils.replaceEach(output, new String[] { " I will " }, new String[] { " you'll " });
         if(in.contains(" my "))
-            temp = StringUtils.replaceEach(temp, new String[] { " my " }, new String[] { " your " });
+            output = StringUtils.replaceEach(output, new String[] { " my " }, new String[] { " your " });
         if(in.contains(" mine "))
-            temp = StringUtils.replaceEach(temp, new String[] { " mine " }, new String[] { " yours " });
+            output = StringUtils.replaceEach(output, new String[] { " mine " }, new String[] { " yours " });
         if(in.contains(" me "))
-            temp = StringUtils.replaceEach(temp, new String[] { " me " }, new String[] { " you " });
-        return temp;
+            output = StringUtils.replaceEach(output, new String[] { " me " }, new String[] { " you " });
+        
+        return output;
     }
 }
 
 
-// "I want a (.*)" - "What would getting " + reflect(m1.group(1)) + " mean to you?"
-// "I need (.*)" - "Why do you need " + reflect(m1.group(1)) + "?"
-// "Can you help me with (.*)" - "Of course, I'd be happy to help with " + reflect(m1.group(1)) + ". What do you need specifically?"
-// "I'm looking for (.*)" - "What would finding " + reflect(m1.group(1)) + " do for you?"
-// "I'm feeling (.*)" - "I'm sorry to hear that you're feeling " + reflect(m1.group(1)) + ". Is there anything I can do to help?"
-// "I want to (.*)" - "What would " + reflect(m1.group(1)) + " accomplish for you?"
+
 // "I want to (.) because (.)" - "What specifically about " + reflect(m1.group(2)) + " makes you want to " + reflect(m1.group(1)) + "?"
-// "I have a problem with (.*)" - "What is the problem you're having with " + reflect(m1.group(1)) + "?"
 // "I'm (.) because (.)" - "How does " + reflect(m1.group(2)) + " make you feel " + reflect(m1.group(1)) + "?"
 // "I (.*) you" - "Why do you " + reflect(m1.group(1)) + " me?"
 // "I (.) (.)" - "Why do you " + reflect(m1.group(1)) + " " + reflect(m1.group(2)) + "?"
-// "Can you (.*)" - "I'm not sure if I can " + reflect(m1.group(1)) + ". Can you please provide more context?"
-// "Are you (.*)" - "I'm an AI, so I do not have the ability to be " + reflect(m1.group(1)) + ". Is there something specific you need help with?"
-// "You are (.*)" - "I'm sorry, I don't understand how you perceive me as " + reflect(m1.group(1)) + ". Can you please elaborate?"
-// "I think (.*)" - "Why do you think " + reflect(m1.group(1)) + "?"
 // "I'm feeling (.) because (.)" - "How does " + reflect(m1.group(2)) + " make you feel " + reflect(m1.group(1)) + "?"
 // "I'm not sure (.*)" - "What specifically are you not sure about " + reflect(m1.group(1)) + "?"
 // "I (.) to (.)" - "What would be the reason for you " + reflect(m1.group(1)) + " " + reflect(m1.group(2)) + "?"
-// "I'm worried about (.*)" - "Why are you worried about " + reflect(m1.group(1)) + "?"
-// "I'm confused about (.*)" - "Can you explain what specifically you're confused about " + reflect(m1.group(1))
